837e83767cae18229b5cf6608893934ea2504b8d
evita-dalmeida <evitadalmeida1@gmail.com>
2020-02-28 12:55:39 -0500

    String 1 in Java

--- CodingBat/string_1.java
+++ CodingBat/string_1.java
@@ -135,3 +135,12 @@ public String extraEnd(String str) {
     return str.substring(0,n) + str.substring(str.length()-n);
   }
   
+//Given a string and an index, return a string length 2 starting at the given index. 
+//If the index is too big or too small to define a string length 2, use the first 2 chars. The string length will be at least 2.
+
+public String twoChar(String str, int index) {
+  if(index<0 || index+2> str.length()) return str.substring(0,2);
+  return str.substring(index,index+2);
+}
+
+//Given a string of odd length, return the string length 3 from its middle, so "Candy" yields "and". The string length will be at least 3.
