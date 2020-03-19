cd9724545bc9db2a100c9876463c8a348b499cc3
evita-dalmeida <evitadalmeida1@gmail.com>
2020-03-19 01:28:38 -0400

    Update functional2.java
    functional 2

--- CodingBat/functional2.java
+++ CodingBat/functional2.java
@@ -42,3 +42,19 @@ public List<String> noLong(List<String> strings) {
   }
 
 //Given a list of strings, return a list of the strings, omitting any string length 3 or 4.
+
+public List<String> no34(List<String> strings) {
+    strings.removeIf(s -> s.length() == 3 || s.length() == 4);
+    return strings;
+  }
+
+//Given a list of strings, return a list where each string has "y" added at its end, omitting any resulting strings that contain "yy" as a substring anywhere.
+
+public List<String> noYY(List<String> strings) {
+    strings.replaceAll(s -> s+"y");
+    strings.removeIf(s -> s.contains("yy"));
+    return strings;
+  }
+
+//Given a list of non-negative integers, return a list of those numbers multiplied by 2, omitting any of the resulting numbers that end in 2.
+
