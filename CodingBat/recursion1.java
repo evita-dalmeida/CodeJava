3e05aeabd5f2a562559941c0abb954cfeba3224d
evita-dalmeida <evitadalmeida1@gmail.com>
2020-03-20 01:17:54 -0400

    Update recursion1.java
    recursion 1

--- CodingBat/recursion1.java
+++ CodingBat/recursion1.java
@@ -30,5 +30,40 @@ public int bunnyEars(int bunnies) {
 //We have bunnies standing in a line, numbered 1, 2, ... The odd bunnies (1, 3, ..) have the normal 2 ears. The even bunnies (2, 4, ..) we'll say have 3 ears, because they each have a raised foot. 
 //Recursively return the number of "ears" in the bunny line 1, 2, ... n (without loops or multiplication).
 
+public int bunnyEars2(int bunnies) {
+    if(bunnies == 0) return 0;
+    if(bunnies%2 == 0){
+      return bunnyEars2(bunnies-1) + 3;
+    }
+    return bunnyEars2(bunnies-1) + 2;
+  }
+  
+//We have triangle made of blocks. The topmost row has 1 block, the next row down has 2 blocks, the next row has 3 blocks, and so on. 
+//Compute recursively (no loops or multiplication) the total number of blocks in such a triangle with the given number of rows.
 
+public int triangle(int rows) {
+    if(rows == 0)
+      return 0;
+    return triangle(rows - 1) + rows;
+  }
+
+//Given a non-negative int n, return the sum of its digits recursively (no loops). Note that mod (%) by 10 yields the rightmost digit (126 % 10 is 6), while divide (/) by 10 removes the rightmost digit (126 / 10 is 12).
+
+public int sumDigits(int n) {
+    if(n == 0)
+      return 0;
+    return n%10 + sumDigits(n/10);
+  }
+
+//Given a non-negative int n, return the count of the occurrences of 7 as a digit, so for example 717 yields 2. (no loops). 
+//Note that mod (%) by 10 yields the rightmost digit (126 % 10 is 6), while divide (/) by 10 removes the rightmost digit (126 / 10 is 12).
+
+public int count7(int n) {
+    if(n == 0)
+      return 0;
+    return  (n%10 == 7?1:0) + count7(n/10);
+  }
+
+//Given a non-negative int n, compute recursively (no loops) the count of the occurrences of 8 as a digit, 
+//except that an 8 with another 8 immediately to its left counts double, so 8818 yields 4. Note that mod (%) by 10 yields the rightmost digit (126 % 10 is 6), while divide (/) by 10 removes the rightmost digit (126 / 10 is 12).
 
