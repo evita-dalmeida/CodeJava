ec7ebc5e5c79d589b43dd98ff27c798e24d6abaf
evita-dalmeida <evitadalmeida1@gmail.com>
2020-03-21 16:14:53 -0400

    Update recursion1.java
    recursion 1

--- CodingBat/recursion1.java
+++ CodingBat/recursion1.java
@@ -156,4 +156,24 @@ public String allStar(String str) {
       return str;
     return str.charAt(0) + "*" + allStar(str.substring(1));
   }
+
+//Given a string, compute recursively a new string where identical chars that are adjacent in the original string are separated from each other by a "*".
+
+public String pairStar(String str) {
+    if(str.length() < 2)
+      return str;
+    if(str.charAt(0) == str.charAt(1))
+      return str.charAt(0) + "*" + pairStar(str.substring(1));
+    return str.charAt(0) + pairStar(str.substring(1));
+  }
+
+//Given a string, compute recursively a new string where all the lowercase 'x' chars have been moved to the end of the string.
+
+public String endX(String str) {
+    if(str.length() < 2)
+    return str;
+    if(str.charAt(0) == 'x')
+      return endX(str.substring(1)) + str.charAt(0);
+    return str.charAt(0) + endX(str.substring(1));
+  }
   
