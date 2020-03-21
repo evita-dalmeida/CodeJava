f52c08fe50b17155ebe73f188a1b429439196658
evita-dalmeida <evitadalmeida1@gmail.com>
2020-03-21 16:01:19 -0400

    Update recursion1.java
    Recursion 1

--- CodingBat/recursion1.java
+++ CodingBat/recursion1.java
@@ -126,3 +126,34 @@ public boolean array6(int[] nums, int index) {
   	return true;
   return array6(nums, index + 1);
 }
+
+//Given an array of ints, compute recursively the number of times that the value 11 appears in the array. We'll use the convention of considering only the part of the array that begins at the given index. 
+//In this way, a recursive call can pass index+1 to move down the array. The initial call will pass in index as 0.
+
+public int array11(int[] nums, int index) {
+    if(nums.length == index)
+      return 0;
+    if(nums[index] == 11)
+      return 1 + array11(nums, index + 1);
+    return array11(nums, index + 1);
+  }
+  
+//Given an array of ints, compute recursively if the array contains somewhere a value followed in the array by that value times 10. We'll use the convention of considering only the part of the array that begins at the given index. 
+//In this way, a recursive call can pass index+1 to move down the array. The initial call will pass in index as 0.
+
+public boolean array220(int[] nums, int index) {
+    if(nums.length == index)
+      return false;
+    if(index > 0 && nums[index-1]*10 == nums[index])
+      return true;
+    return array220(nums, index+1);
+  }
+
+//Given a string, compute recursively a new string where all the adjacent chars are now separated by a "*".
+
+public String allStar(String str) {
+    if(str.length() <= 1)
+      return str;
+    return str.charAt(0) + "*" + allStar(str.substring(1));
+  }
+  
