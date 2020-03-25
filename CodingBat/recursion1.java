4aa9be870e5f810649061122c710a1a61671c2b5
evita-dalmeida <evitadalmeida1@gmail.com>
2020-03-24 21:45:22 -0400

    Update recursion1.java
    recursion 1

--- CodingBat/recursion1.java
+++ CodingBat/recursion1.java
@@ -250,3 +250,13 @@ public String parenBit(String str) {
   }
 
 //Given a string, return true if it is a nesting of zero or more pairs of parenthesis, like "(())" or "((()))". Suggestion: check the first and last chars, and then recur on what's inside them.
+
+public boolean nestParen(String str) {
+    if(str.length() == 0)
+      return true;
+    if(str.length()>=2 && str.charAt(0) == '(' && str.charAt(str.length()-1) == ')'){
+      return nestParen(str.substring(1,str.length()-1));
+    }
+    return false;
+  }
+  
