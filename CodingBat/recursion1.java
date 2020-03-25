862c8144510608d4d3132b89bde1025cf71a14c2
evita-dalmeida <evitadalmeida1@gmail.com>
2020-03-24 21:55:48 -0400

    Update recursion1.java
    strCopies

--- CodingBat/recursion1.java
+++ CodingBat/recursion1.java
@@ -269,4 +269,15 @@ public int strCount(String str, String sub) {
       return 1 + strCount(str.substring(sub.length()),sub);
     return strCount(str.substring(1),sub);
   }
+
+//Given a string and a non-empty substring sub, compute recursively if at least n copies of sub appear in the string somewhere, possibly with overlapping. N will be non-negative.
+
+public boolean strCopies(String str, String sub, int n) {
+    if(str.length() == 0)
+      return n == 0;
+    if(str.length()>= sub.length() && str.substring(0,sub.length()).equals(sub)){
+      return strCopies(str.substring(1),sub, n-1);
+    }
+    return strCopies(str.substring(1),sub, n);
+  }
   
