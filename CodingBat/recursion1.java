593be181efe48603c2ebeffca4820676e0221cf8
evita-dalmeida <evitadalmeida1@gmail.com>
2020-03-24 21:48:55 -0400

    Update recursion1.java
    strCount recursion 1

--- CodingBat/recursion1.java
+++ CodingBat/recursion1.java
@@ -259,4 +259,14 @@ public boolean nestParen(String str) {
     }
     return false;
   }
+
+//Given a string and a non-empty substring sub, compute recursively the number of times that sub appears in the string, without the sub strings overlapping.
+
+public int strCount(String str, String sub) {
+    if(str.length() == 0)
+      return 0;
+    if(str.length()>= sub.length() && str.substring(0,sub.length()).equals(sub))
+      return 1 + strCount(str.substring(sub.length()),sub);
+    return strCount(str.substring(1),sub);
+  }
   
